98bcc8fae52edc04ab714d951446ea8d7736650e
Noor Mohammad <noormohammad@AAOMAN.COM>
2022-10-10 16:45:43 +0400

    header updted

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,22 @@ import { headerData } from "../constants";
 
 const Header = () => {
   return (
-    <div className="flex justify-between max-w-screen-xl mx-auto">
-      <div>
-        <img className="w-44" src={headerLogoImg} alt="headerLogoImg" />
-      </div>
-      <div className="flex items-center">
-        {headerData.map((item) => (
-          <div key={item.id}>
-            <h4 className="px-4 font-sans text-[14px] uppercase font-semibold">
-              {item.title}
-            </h4>
-          </div>
-        ))}
+    <div className="w-full h-auto shadow-lg py-3">
+      <div className="flex justify-between max-w-screen-xl mx-auto">
+        <div>
+          <img className="w-44" src={headerLogoImg} alt="headerLogoImg" />
+        </div>
+        <div className="flex items-center gap-4">
+          {headerData.map((item) => (
+            <div key={item.id}>
+              <h4 className="relative px-2 font-sans text-[14px] uppercase font-semibold group cursor-pointer">
+                {item.title}
+                <span className="w-full h-[2px] -bottom-1 left-0 bg-red-800 absolute hidden group-hover:inline-block transform duration-300 ease-in-out  bg-gradient-to-r from-green-[#E80566] to-blue-[#1C75BC]"></span>
+                {/* bg-[linear-gradient(95.68deg, #E80566 0.05%, #1C75BC 102.79%] */}
+              </h4>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
